dc035140615d64e85bf1bd0e9a0000cb9d8621c9
ayushmishra <mail81mitthu@gmail.com>
2023-11-12 09:19:35 +0530

    updated st theme

--- .config/st/config.h
+++ .config/st/config.h
@@ -96,24 +96,34 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	"#18b518", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	/* more colors can be added after 255 to use with DefaultXX */
+  /* 8 normal colors */
+     	"black",
+     	"red",
+     	"green",
+     	"yellow",
+     	"blue",
+     	"magenta",
+     	"cyan",
+     	"gray90",
+     
+     	/* 8 bright colors */
+     	"gray50",
+     	"red",
+     	"green",
+     	"yellow",
+     	"#5c5cff",
+     	"magenta",
+     	"cyan",
+     	"white",
+     
+     	[255] = 0,
+     
+     	/* more colors can be added after 255 to use with DefaultXX */
+     	// "#cccccc",
+     	// "#555555",
+     	// "black", /* default background colour */
+     	// "black", /* default background colour */
+
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
 	"#0a0a0a", /* 258 -> bg */
