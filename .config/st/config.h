3c43885e18608f67597fc29f40257b8f1cdbf98e
ayush mishra <mail81mitthu@gmail.com>
2024-12-03 09:41:17 +0530

    m: changed st bg opacity

--- .config/st/config.h
+++ .config/st/config.h
@@ -110,7 +110,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.65;
+float alpha = 0.45;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
